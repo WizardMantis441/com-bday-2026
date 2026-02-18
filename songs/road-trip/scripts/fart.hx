6fc5e61e54162ea8cda68ccccfa974a7c44d7388
Wizard <77557487+WizardMantis441@users.noreply.github.com>
2026-02-18 12:11:27 -0600

    road trip ending and icons and another thing i tnhink

--- songs/road-trip/scripts/fart.hx
+++ songs/road-trip/scripts/fart.hx
@@ -1,4 +1,27 @@
+var bg:FunkinSprite;
+var ending:FunkinSprite;
+
 function postCreate() {
+    bg = new FunkinSprite(0, 0).makeSolid(FlxG.width, FlxG.height, 0xA0000000);
+    bg.scrollFactor.set();
+    bg.zoomFactor = 0;
+    bg.alpha = 0;
+    add(bg);
+    
+    ending = new FunkinSprite(0, 0, Paths.image('stages/roadtrip/End3DS'));
+    ending.addAnim("1", "End3DS P10", 12, true);
+    ending.addAnim("2", "End3DS P20", 12, true);
+    ending.addAnim("3", "End3DS P30", 12, true);
+    ending.addAnim("4", "End3DS P40", 12, true);
+    ending.playAnim("1");
+    ending.scrollFactor.set();
+    ending.zoomFactor = 0;
+    ending.screenCenter();
+    ending.scale.set(2, 2);
+    ending.visible = false;
+    add(ending);
+
+
     for (i in [iconP1, iconP2, healthBar, healthBarBG, scoreTxt, missesTxt, accuracyTxt])
         i.visible = false;
 }
@@ -11,4 +34,16 @@ function onNoteHit(event:NoteHitEvent) {
         case "victor": strumLines.members[0].characters[3];
         default: boyfriend;
     }
+}
+
+function fade() {
+    for (strum in strumLines.members[1].members) {
+        FlxTween.tween(strum, {alpha: 0}, 1);
+        FlxTween.tween(bg, {alpha: 1}, 1);
+    }
+}
+
+function cutscene(which) {
+    ending.visible = true;
+    ending.playAnim(which);
 }
